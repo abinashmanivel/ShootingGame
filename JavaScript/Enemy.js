0a428e5a28251a1d897a413230d90713727ebf6f
Shashwat Shashank <shaswat97@gmail.com>
2023-03-30 00:22:47 +0530

    Update Enemy.js
    updated enemy.js

--- JavaScript/Enemy.js
+++ JavaScript/Enemy.js
@@ -1,21 +1,22 @@
+import { c } from "./script.js";
 export default class Enemy {
-    constructor(x, y, radius, color, velocity, speed) {
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.color = color;
-      this.velocity = velocity;
-      this.speed = speed;
-    }
-    draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-    }
-    update() {
-      this.draw();
-      this.x = this.x + this.velocity.x * this.speed;
-      this.y = this.y + this.velocity.y * this.speed;
-    }
+  constructor(x, y, radius, color, velocity, speed) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.speed = speed;
   }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x * this.speed;
+    this.y = this.y + this.velocity.y * this.speed;
+  }
+}
